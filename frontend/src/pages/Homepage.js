1afc4f683b00d9e6009434b781708ade1274ba38
Aidan Matchette <frigasack@gmail.com>
2022-04-16 11:50:17 -0700

    homepage complete -no navbar

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -24,21 +24,37 @@ const Homepage = () => {
                         <div className='header-text'>
                             <h1>Create your Crytpo portfolio</h1>
                             <h5 className='description'>Start tracking all of your crypto assests securly in a matter of minutes!</h5>
-                            <Button size='large' variant="contained" href="/signup" 
+                            <Button size='large' variant="contained" href="#/signup" 
                             sx={{color: 'white', background: 'purple', borderRadius: "10px", ":hover": {background: '#46016b'} }} disableElevation>
                             Create an account</Button>
                         </div>
                         <div className='header-content'>
                             <img src="https://images.unsplash.com/photo-1640340434855-6084b1f4901c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" width="600" height="500"/>
                         </div>
-                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className='dashboard-section hp-section'>
+            <div className='dashboard-container hp-container'>
+                <div className='dashboard-flex'>
+                    <div className='dashboard-content'>
+                        <div className='dashboard-image'>
+                            <img src="https://images.unsplash.com/photo-1621761191319-c6fb62004040?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" width="600" height="500"/>
+                        </div> 
+                        <div className='dashboard-text'>
+                            <h2 className='dashboard'>Dashboard</h2>
+                            <h4 className='dashboard-description'>See what the current trends of your favorite Crypto coins are doing!</h4>
+                            <Button size='large' variant="contained" href="#/dashboard" 
+                            sx={{color: 'white', background: 'purple', borderRadius: "10px", ":hover": {background: '#46016b'} }} disableElevation>
+                            Dashboard</Button>
+                        </div>
 
                     </div>
                 </div>
             </div>
         </div>
-        <div className='sign-up'>
-        </div>
+ 
     </>
   )
 }
